4fb8a66ea129fb099c70c749e828b3702da6a02d
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2024-02-14 08:58:47 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+let pregunta = 0
+input.onGesture(Gesture.Shake, function () {
+    pregunta = randint(1, 3)
+    if (pregunta == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            `)
+    } else if (pregunta == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            # # . # #
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    }
+})
